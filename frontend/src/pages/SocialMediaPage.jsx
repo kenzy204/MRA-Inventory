b2cb4d367f88961f752e1503c3373f6f7fecc3e5
kenzy204 <kenzysherif204@gmail.com>
2026-04-15 13:36:20 +0200

    Create SocialMediaPage.jsx

---
+++ frontend/src/pages/SocialMediaPage.jsx
@@ -0,0 +1,8 @@
+export default function SocialMediaPage() {
+  return (
+    <div className="panel">
+      <h2>Social media</h2>
+      <p>Beheer hier je social media links.</p>
+    </div>
+  );
+}
