4b34c94fbdd89503b3e47adfe89c6ee6c7cffbfd
kenzy204 <kenzysherif204@gmail.com>
2026-04-15 18:01:49 +0200

    Update clientRoutes.js

--- backend/src/routes/clientRoutes.js
+++ backend/src/routes/clientRoutes.js
@@ -9,6 +9,7 @@ router.use(authMiddleware);
 router.get('/', controller.getClients);
 router.get('/:id', controller.getClient);
 router.post('/', controller.createClient);
+router.put('/:id', controller.updateClient);
 router.delete('/:id', controller.deleteClient);
 
 module.exports = router;
