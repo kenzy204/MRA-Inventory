1cd89213b12abbf5ca8038a20d471b7454518203
kenzy204 <kenzysherif204@gmail.com>
2026-04-15 13:35:41 +0200

    Create FietsverzekeringPage.jsx

---
+++ frontend/src/pages/FietsverzekeringPage.jsx
@@ -0,0 +1,8 @@
+export default function FietsverzekeringPage() {
+  return (
+    <div className="panel">
+      <h2>Fietsverzekering</h2>
+      <p>Verzekering tools en informatie volgen binnenkort.</p>
+    </div>
+  );
+}
