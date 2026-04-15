78fa3b54b3fcec6757013c50f18e591d108293ec
kenzy204 <kenzysherif204@gmail.com>
2026-04-15 13:36:03 +0200

    Create ChangePasswordPage.jsx

---
+++ frontend/src/pages/ChangePasswordPage.jsx
@@ -0,0 +1,8 @@
+export default function ChangePasswordPage() {
+  return (
+    <div className="panel">
+      <h2>Wachtwoord wijzigen</h2>
+      <p>Hier komt je change password formulier.</p>
+    </div>
+  );
+}
