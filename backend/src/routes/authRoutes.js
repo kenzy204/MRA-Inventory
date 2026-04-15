7ab3dc7fd68e183620c83bbd247c648ca3af6f32
kenzy204 <kenzysherif204@gmail.com>
2026-04-15 13:54:23 +0200

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { login } = require('../controllers/authController');
+const { login, changePassword } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/login', login);
+router.put('/change-password', authMiddleware, changePassword);
 
 module.exports = router;
