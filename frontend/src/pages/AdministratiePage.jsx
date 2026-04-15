8d57ec727561753ae48e5741932e9a1900228dc6
kenzy204 <kenzysherif204@gmail.com>
2026-04-15 13:35:15 +0200

    Create AdministratiePage.jsx

---
+++ frontend/src/pages/AdministratiePage.jsx
@@ -0,0 +1,8 @@
+export default function AdministratiePage() {
+  return (
+    <div className="panel">
+      <h2>Administratie</h2>
+      <p>Hier komt binnenkort een overzicht van administratie en rapportages.</p>
+    </div>
+  );
+}
